bc0739503b6067f375b244b83ce23b06fc80b119
veryspry <ehlinger.matt@gmail.com>
2021-01-29 16:08:37 -0600

    feat: create BackupRepo for each site the first time a backup is created for a given Provider

--- src/main/hubQueries.ts
+++ src/main/hubQueries.ts
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 import type { Site } from '@getflywheel/local';
 import { getServiceContainer } from '@getflywheel/local/main';
-import type { Providers, RcloneConfig, BackupSite } from '../types';
+import type { Providers, RcloneConfig, BackupSite, BackupRepo } from '../types';
 
 /* @ts-ignore */
 const { localHubClient } = getServiceContainer().cradle;
@@ -62,4 +62,63 @@ export async function createBackupSite (site: Site): Promise<BackupSite> {
 	});
 
 	return data?.createBackupSite;
+}
+
+export async function createBackupRepo (site: Site, provider: Providers): Promise<BackupRepo> {
+	const { data } = await localHubClient.mutation({
+		mutation: gql`
+			mutation createBackupRepo(
+				$siteID: Int!,
+				$providerID: String!,
+				$repoID: String!
+			) {
+				createBackupRepo(
+					site_id: $siteID,
+					provider_id: $providerID,
+					hash: $repoID
+				) {
+					id
+					site_id
+					provider_id
+					hash
+				}
+	  		}
+		`,
+		variables: {
+			siteID: site.id,
+			providerID: provider,
+			/* @ts-ignore */
+			repoID: site.localBackupRepoID,
+		},
+	});
+
+	return {
+		...data?.createBackupRepo,
+		providerID: data?.createBackupRepo?.provider_id,
+		siteID: data?.createBackupRepo?.site_id,
+	};
+}
+
+export async function getBackupReposByProviderID (provider: Providers): Promise<BackupRepo[]> {
+	const { data } = await localHubClient.query({
+		query: gql`
+			query getBackupReposByProviderID($providerID: String) {
+  				backupRepos(provider_id: $providerID) {
+    				id
+    				site_id
+    				provider_id
+    				hash
+  				}
+			}
+		`,
+		variables: {
+			providerID: provider,
+		},
+	});
+
+	return {
+		...data?.backupRepos,
+		providerID: data?.backupRepos?.provider_id,
+		siteID: data?.backupRepos?.site_id,
+	};
 }
