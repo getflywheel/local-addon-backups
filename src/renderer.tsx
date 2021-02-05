0b93983dbe8b84d62df13ca91b026c9aa07443d5
veryspry <ehlinger.matt@gmail.com>
2021-02-05 16:50:20 -0600

    wip: add some buttons to facilitate testing

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -10,21 +10,23 @@ const titlize = (a: string) => startCase(a.toLowerCase());
 interface RowProps<T> {
 	onClick: (a: T, provider: Providers) => void;
 	buttonText: string;
-	description: string;
+	description?: string;
 }
 
 const Row = (props: RowProps<void>) => (
-	<>
+	<div>
 		<TextButton
 			style={{ paddingLeft: 0 }}
 			onClick={props.onClick}
 		>
 			{props.buttonText}
 		</TextButton>
-		<p>
-			<small>{props.description}</small>
-		</p>
-	</>
+		{props.description ? (
+			<p>
+				<small>{props.description}</small>
+			</p>
+		) : null}
+	</div>
 );
 
 const MainComponent = (props: { rows: RowProps<SiteJSON>[], site: SiteJSON }) => {
@@ -41,9 +43,9 @@ const MainComponent = (props: { rows: RowProps<SiteJSON>[], site: SiteJSON }) =>
 			<p>
 				<small>Select which provider you would like to backup to</small>
 			</p>
-			{rows.map(({ onClick, buttonText, description }) => (
+			{rows.map(({ onClick, buttonText, description }, i) => (
 				<Row
-					key={buttonText + description}
+					key={`buttonText-${i}`}
 					onClick={() => onClick(site, provider)}
 					buttonText={buttonText}
 					description={description}
@@ -53,6 +55,8 @@ const MainComponent = (props: { rows: RowProps<SiteJSON>[], site: SiteJSON }) =>
 	);
 };
 
+const expandTilde = (path: string) => path.replace(/^~/, '/home/matt');
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function (context): void {
 	const { hooks } = context;
@@ -60,14 +64,8 @@ export default function (context): void {
 	const rows = [
 		{
 			onClick: async (site, provider) => {
-				try {
-
-					const result = await ipcAsync('start-site-backup', site.id, provider);
-
-					console.log('result from backup..........', result);
-				} catch (err) {
-					console.log(err, err.messageMain, err.stackMain)
-				}
+				const result = await ipcAsync('start-site-backup', site.id, provider);
+				console.log('result from backup..........', result);
 			},
 			buttonText: 'Create Snapshot Now',
 			description: 'Take a snapshot of the current project and upload it to the cloud.',
@@ -77,8 +75,43 @@ export default function (context): void {
 				const snapshots = await ipcAsync('list-site-snapshots', site.id, provider);
 				console.log('snapshots...', snapshots);
 			},
-			buttonText: 'List Site Backups',
+			buttonText: 'List Site Snapshots',
 			description: 'Query for all site backups (this might take a few seconds).',
+		},
+		{
+			onClick: async (site, provider) => {
+				console.log(
+					await ipcAsync('list-repos', site.id, provider),
+				);
+			},
+			buttonText: 'List Repos',
+			description: 'Lists repos for all sites',
+		},
+		{
+			onClick: async (site, provider) => {
+				console.log(site)
+				const { localBackupRepoID } = site;
+				const bin = 'restic';
+				const flags = [
+					// '--fast-list',
+					// '--use-json-log',
+					'--json',
+				];
+				// const cmd = `backup --repo rclone:google:${localBackupRepoID} ${flags.join(' ')} \"${expandTilde(site.path)}\"`;
+				const rcloneArgsForRestic = `-o rclone.args="serve restic --stdio --verbose"`;
+				/**
+				 * the double colon is an attempt at using the rclone :backend: syntax to dynamically use a backend without a named
+				 * config in the rclone config. The value of :backend: should be whatever would be set in the type field in the rclone config
+				 */
+				const cmd = `${rcloneArgsForRestic} --repo rclone::drive:${localBackupRepoID} ${flags.join(' ')} init`;
+
+				console.log(
+					await ipcAsync('do-arbitrary-shit', site.id, provider, bin, cmd),
+				);
+
+			},
+			buttonText: 'Run the command',
+			description: 'runs whatever command is defined on the front end for my ease',
 		},
 	];
 
