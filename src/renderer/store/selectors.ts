2cb98326386dad72f70c812e4ea77ba4c88720f7
veryspry <ehlinger.matt@gmail.com>
2021-02-18 13:10:03 -0600

    feat: store enabled providers in redux

--- src/renderer/store/selectors.ts
+++ src/renderer/store/selectors.ts
@@ -1,8 +1,12 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { store } from './store';
+import type { HubProviderRecord } from '../../types';
 
 const activeSiteID = (state) => state.activeSiteID;
 
+const enabledProviders = (state) => state.enabledProviders;
+
 export const selectors = {
 	activeSiteID: (): string => activeSiteID(store.getState()),
+	enabledProviders: (): HubProviderRecord[] => enabledProviders(store.getState()),
 };
