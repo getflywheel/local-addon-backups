f039dde3d9d4d44580a5092992faf6ca0a850124
tylerhal <tylerhhal@gmail.com>
2021-06-04 11:05:32 -0500

    refactor: use bang instead of equality

--- src/renderer/components/modals/BackupCloneContents.tsx
+++ src/renderer/components/modals/BackupCloneContents.tsx
@@ -66,7 +66,7 @@ export const BackupCloneContents = (props: ModalContentsProps) => {
 				<PrimaryButton
 					style={{ marginTop: 0 }}
 					onClick={() => onModalSubmit()}
-					disabled={inputSiteNameData === ''}
+					disabled={!inputSiteNameData}
 				>
 					Create Clone
 				</PrimaryButton>
