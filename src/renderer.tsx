884d86910d9e61a73fe1f40f9be53e60200d4a37
Benjamin Turner <benjamin@passionsplay.com>
2022-04-13 08:33:13 -0700

    refactor: Save renderer with prettier rules

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,26 +1,20 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { ApolloProvider } from "@apollo/client";
-import { RestoreStates, BackupStates } from "./types";
-import { store, actions } from "./renderer/store/store";
-import SiteInfoToolsSection from "./renderer/components/siteinfotools/SiteInfoToolsSection";
-import { setupListeners } from "./renderer/helpers/setupListeners";
-import { client } from "./renderer/localClient/localGraphQLClient";
-import { ChooseCreateSite } from "./renderer/components/multimachinebackups/ChooseCreateSite";
-import { SelectSiteBackup } from "./renderer/components/multimachinebackups/SelectSiteBackup";
-import { SelectSnapshot } from "./renderer/components/multimachinebackups/SelectSnapshot";
-import { CloseButtonWithStore } from "./renderer/components/multimachinebackups/CloseButtonWithStore";
-import * as LocalRenderer from "@getflywheel/local/renderer";
-import {
-	Stepper,
-	Step,
-	Text,
-	TextButtonExternal,
-	Tooltip,
-} from "@getflywheel/local-components";
-import { LOCAL_ROUTES } from "./constants";
-import PromoBanner from "./renderer/components/PromoBanner";
-import CreateSiteRadioOption from "./renderer/components/CreateSiteRadioOption";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { ApolloProvider } from '@apollo/client';
+import { RestoreStates, BackupStates } from './types';
+import { store, actions } from './renderer/store/store';
+import SiteInfoToolsSection from './renderer/components/siteinfotools/SiteInfoToolsSection';
+import { setupListeners } from './renderer/helpers/setupListeners';
+import { client } from './renderer/localClient/localGraphQLClient';
+import { ChooseCreateSite } from './renderer/components/multimachinebackups/ChooseCreateSite';
+import { SelectSiteBackup } from './renderer/components/multimachinebackups/SelectSiteBackup';
+import { SelectSnapshot } from './renderer/components/multimachinebackups/SelectSnapshot';
+import { CloseButtonWithStore } from './renderer/components/multimachinebackups/CloseButtonWithStore';
+import * as LocalRenderer from '@getflywheel/local/renderer';
+import { Stepper, Step, Text, TextButtonExternal, Tooltip } from '@getflywheel/local-components';
+import { LOCAL_ROUTES } from './constants';
+import PromoBanner from './renderer/components/PromoBanner';
+import CreateSiteRadioOption from './renderer/components/CreateSiteRadioOption';
 
 setupListeners();
 
@@ -41,25 +35,17 @@ const withStoreProvider = (Component) => (props) =>
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function (context): void {
 	const { hooks } = context;
-	const SiteInfoToolsSectionHOC = withApolloProvider(
-		withStoreProvider(SiteInfoToolsSection)
-	);
-	const ChooseCreateSiteHOC = withApolloProvider(
-		withStoreProvider(ChooseCreateSite)
-	);
-	const SelectSiteBackupHOC = withApolloProvider(
-		withStoreProvider(SelectSiteBackup)
-	);
-	const SelectSnapshotHOC = withApolloProvider(
-		withStoreProvider(SelectSnapshot)
-	);
+	const SiteInfoToolsSectionHOC = withApolloProvider(withStoreProvider(SiteInfoToolsSection));
+	const ChooseCreateSiteHOC = withApolloProvider(withStoreProvider(ChooseCreateSite));
+	const SelectSiteBackupHOC = withApolloProvider(withStoreProvider(SelectSiteBackup));
+	const SelectSnapshotHOC = withApolloProvider(withStoreProvider(SelectSnapshot));
 	const CloseButtonHOC = withStoreProvider(CloseButtonWithStore);
 
-	hooks.addFilter("siteInfoToolsItem", (items) => {
+	hooks.addFilter('siteInfoToolsItem', (items) => {
 		const cloudBackupItems = [
 			{
-				path: "/localBackups",
-				menuItem: "Cloud Backups",
+				path: '/localBackups',
+				menuItem: 'Cloud Backups',
 				render: ({ site }) => <SiteInfoToolsSectionHOC site={site} />,
 			},
 		];
@@ -71,11 +57,8 @@ export default function (context): void {
 		return cloudBackupItems;
 	});
 
-	hooks.addFilter("allowedSiteOverlayStatuses", (statuses: string[]) => {
-		const cloudBackupStatuses: string[] = [
-			...Object.values(RestoreStates),
-			BackupStates.creatingDatabaseSnapshot,
-		];
+	hooks.addFilter('allowedSiteOverlayStatuses', (statuses: string[]) => {
+		const cloudBackupStatuses: string[] = [...Object.values(RestoreStates), BackupStates.creatingDatabaseSnapshot];
 
 		statuses.forEach((status) => {
 			cloudBackupStatuses.push(status);
@@ -87,26 +70,26 @@ export default function (context): void {
 	/**
 	 * Add CloudBackups as an option when creating a new site
 	 */
-	hooks.addFilter("CreateSite:RadioOptions", (options) => {
+	hooks.addFilter('CreateSite:RadioOptions', (options) => {
 		return {
 			...options,
-			"add-site/select-site-backup": CreateSiteRadioOption(),
+			'add-site/select-site-backup': CreateSiteRadioOption(),
 		};
 	});
 
 	/**
 	 * Add CloudBackups as an option when creating a new site
 	 */
-	hooks.addFilter("CreateSite:Routes", (routes) => {
+	hooks.addFilter('CreateSite:Routes', (routes) => {
 		const cloudBackupRoutes = [
 			{
-				key: "add-site-select-site-backup",
-				path: "/main/add-site",
+				key: 'add-site-select-site-backup',
+				path: '/main/add-site',
 				component: SelectSiteBackupHOC,
 			},
 			{
-				key: "add-site-select-snapshot",
-				path: "/main/add-site/select-snapshot",
+				key: 'add-site-select-snapshot',
+				path: '/main/add-site/select-snapshot',
 				component: SelectSnapshotHOC,
 			},
 		];
@@ -115,20 +98,20 @@ export default function (context): void {
 
 	// add new routes and components to Local core
 
-	hooks.addFilter("AddSiteIndexJS:RoutesArray", (routes, path) => {
+	hooks.addFilter('AddSiteIndexJS:RoutesArray', (routes, path) => {
 		const cloudBackupRoutes = [
 			{
-				key: "add-site-choose",
+				key: 'add-site-choose',
 				path: `${path}/`,
 				component: ChooseCreateSiteHOC,
 			},
 			{
-				key: "add-site-select-site-backup",
+				key: 'add-site-select-site-backup',
 				path: LOCAL_ROUTES.ADD_SITE_BACKUP_SITE,
 				component: SelectSiteBackupHOC,
 			},
 			{
-				key: "add-site-select-snapshot",
+				key: 'add-site-select-snapshot',
 				path: LOCAL_ROUTES.ADD_SITE_BACKUP_SNAPSHOT,
 				component: SelectSnapshotHOC,
 			},
@@ -136,7 +119,7 @@ export default function (context): void {
 		routes.forEach((route) => {
 			if (route.path === `${path}/`) {
 				cloudBackupRoutes.push({
-					key: "add-site-add",
+					key: 'add-site-add',
 					path: LOCAL_ROUTES.ADD_SITE_CREATE_NEW,
 					component: route.component,
 				});
@@ -147,94 +130,66 @@ export default function (context): void {
 		return cloudBackupRoutes;
 	});
 
-	hooks.addContent("CreateSite_Messages", () => <PromoBanner />);
+	hooks.addContent('CreateSite_Messages', () => <PromoBanner />);
 
 	// optionally modify NewSiteEnvironment component functionality in Local core
-	hooks.addFilter(
-		"AddSiteIndexJS:NewSiteEnvironment",
-		(newSiteEnvironmentProps) => {
-			if (
-				newSiteEnvironmentProps.siteSettings.cloudBackupMeta
-					?.createdFromCloudBackup
-			) {
-				const continueCreateSite = () => {
-					LocalRenderer.sendIPCEvent("addSite", {
-						newSiteInfo: newSiteEnvironmentProps.siteSettings,
-						goToSite: true,
-						installWP: false,
-					});
+	hooks.addFilter('AddSiteIndexJS:NewSiteEnvironment', (newSiteEnvironmentProps) => {
+		if (newSiteEnvironmentProps.siteSettings.cloudBackupMeta?.createdFromCloudBackup) {
+			const continueCreateSite = () => {
+				LocalRenderer.sendIPCEvent('addSite', {
+					newSiteInfo: newSiteEnvironmentProps.siteSettings,
+					goToSite: true,
+					installWP: false,
+				});
 
-					// Reset the user selected options from the add site flow.
-					store.dispatch(actions.resetMultiMachineRestoreState());
-				};
+				// Reset the user selected options from the add site flow.
+				store.dispatch(actions.resetMultiMachineRestoreState());
+			};
 
-				const onGoBack = () => {
-					LocalRenderer.sendIPCEvent(
-						"goToRoute",
-						LOCAL_ROUTES.ADD_SITE_BACKUP_SNAPSHOT
-					);
-				};
-
-				return {
-					...newSiteEnvironmentProps,
-					onContinue: continueCreateSite,
-					onGoBack,
-					buttonText: "Add Site",
-				};
-			}
+			const onGoBack = () => {
+				LocalRenderer.sendIPCEvent('goToRoute', LOCAL_ROUTES.ADD_SITE_BACKUP_SNAPSHOT);
+			};
 
 			return {
 				...newSiteEnvironmentProps,
-				onGoBack: () =>
-					LocalRenderer.sendIPCEvent(
-						"goToRoute",
-						LOCAL_ROUTES.ADD_SITE_CREATE_NEW
-					),
+				onContinue: continueCreateSite,
+				onGoBack,
+				buttonText: 'Add Site',
 			};
 		}
-	);
+
+		return {
+			...newSiteEnvironmentProps,
+			onGoBack: () => LocalRenderer.sendIPCEvent('goToRoute', LOCAL_ROUTES.ADD_SITE_CREATE_NEW),
+		};
+	});
 
 	// add a new breadcrumbs stepper to the Add Site user flow
-	hooks.addFilter("AddSiteIndexJS:RenderBreadcrumbs", (breadcrumbsData) => {
+	hooks.addFilter('AddSiteIndexJS:RenderBreadcrumbs', (breadcrumbsData) => {
 		const { localHistory, siteSettings } = breadcrumbsData;
 		const cloudBackupStepper = () => (
 			<Stepper>
 				<Step
-					key={"choose-site"}
+					key={'choose-site'}
 					number={1}
-					done={
-						localHistory.location.pathname !==
-						LOCAL_ROUTES.ADD_SITE_BACKUP_SITE
-					}
-					active={
-						localHistory.location.pathname ===
-						LOCAL_ROUTES.ADD_SITE_BACKUP_SITE
-					}
+					done={localHistory.location.pathname !== LOCAL_ROUTES.ADD_SITE_BACKUP_SITE}
+					active={localHistory.location.pathname === LOCAL_ROUTES.ADD_SITE_BACKUP_SITE}
 				>
 					Select Site and Name
 				</Step>
 				<Step
-					key={"choose-snapshot"}
+					key={'choose-snapshot'}
 					number={2}
-					done={
-						localHistory.location.pathname ===
-						LOCAL_ROUTES.ADD_SITE_ENVIRONMENT
-					}
-					active={
-						localHistory.location.pathname ===
-						LOCAL_ROUTES.ADD_SITE_BACKUP_SNAPSHOT
-					}
+					done={localHistory.location.pathname === LOCAL_ROUTES.ADD_SITE_ENVIRONMENT}
+					active={localHistory.location.pathname === LOCAL_ROUTES.ADD_SITE_BACKUP_SNAPSHOT}
 				>
 					Select Backup
 				</Step>
 				<Step
-					key={"choose-environment"}
+					key={'choose-environment'}
 					number={3}
 					done={false}
-					active={
-						localHistory.location.pathname ===
-						LOCAL_ROUTES.ADD_SITE_ENVIRONMENT
-					}
+					active={localHistory.location.pathname === LOCAL_ROUTES.ADD_SITE_ENVIRONMENT}
 				>
 					Set Up Environment
 				</Step>
