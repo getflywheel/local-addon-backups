1b4537dda0d78faf2076db32825020dc891c2663
Benjamin Turner <benjamin@passionsplay.com>
2022-04-13 08:49:08 -0700

    chore: delete unused component CloudBackupsRadioOption.jsx

--- src/renderer/components/CloudBackupsRadioOption.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from "@getflywheel/local-components";
-
-const CloudBackupsRadioOption = (props) => {
-	return <Text>Cloud Backups Radio Option.</Text>;
-};
-
-export default CloudBackupsRadioOption;
