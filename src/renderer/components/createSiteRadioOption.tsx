dc5c0ea4edd3405ebde2078b4f454035bad0da36
Benjamin Turner <benjamin@passionsplay.com>
2022-04-13 12:34:23 -0700

    feat: Add Backup option to Core's CreateSite options filter

---
+++ src/renderer/components/createSiteRadioOption.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Container, Text, TextButtonExternal, Tooltip } from '@getflywheel/local-components';
+import { $hub } from '@getflywheel/local/renderer';
+
+/*
+ * Defines the RadioItem that Local Core uses within the RadioBlock during
+ * the CreateSite flow.
+ */
+const CreateSiteRadioOption = () => {
+	const isLoggedOut = $hub.user === null;
+
+	return {
+		label: 'Create from a Cloud Backup',
+		key: 'create-site-option-from-backup',
+		disabled: isLoggedOut,
+
+		container: isLoggedOut && {
+			element: (
+				<Tooltip
+					showDelay={2}
+					content={
+						<Container margin="s">
+							<Text tag="p">
+								Uh oh!
+								<br />
+								You need to log into your Local Account to use Cloud Backups.
+							</Text>
+							<TextButtonExternal
+								onClick={() => {
+									$hub.login();
+								}}
+							>
+								Log into Local Account
+							</TextButtonExternal>
+						</Container>
+					}
+					popperOffsetModifier={{
+						offset: [0, 12],
+					}}
+					position="top"
+				/>
+			),
+		},
+
+		content: (
+			<>
+				<Text>Pull a saved site down to Local from Google Drive or Dropbox.</Text>
+				<TextButtonExternal
+					onClick={(evt: Event) => {
+						evt.stopPropagation();
+					}}
+					inline={false}
+					style={{ paddingTop: 7 }}
+					href="https://localwp.com/help-docs/local-add-ons-help/cloud-backups/"
+				>
+					What&apos;s this?
+				</TextButtonExternal>
+			</>
+		),
+	};
+};
+
+export default CreateSiteRadioOption;
