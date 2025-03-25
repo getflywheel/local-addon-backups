c9162f55beba2f727d98366b634049202b6365da
joshuapregua <joshua.pregua@wpengine.com>
2025-03-25 09:58:47 +0800

    chore(bump): bumped jest to latest version

--- src/main/ensureBins.test.ts
+++ src/main/ensureBins.test.ts
@@ -7,18 +7,18 @@ describe('Bundled binaries', () => {
 	it('includes rclone for supported os\'s', () => {
 		const vendorDir = path.join(__dirname, '..', '..', 'vendor');
 
-		expect(fs.existsSync(path.join(vendorDir, 'darwin', 'rclone'))).toBeTrue();
-		expect(fs.existsSync(path.join(vendorDir, 'linux', 'rclone'))).toBeTrue();
-		expect(fs.existsSync(path.join(vendorDir, 'win32', 'rclone.exe'))).toBeTrue();
-		expect(fs.existsSync(path.join(vendorDir, 'win64', 'rclone.exe'))).toBeTrue();
+		expect(fs.existsSync(path.join(vendorDir, 'darwin', 'rclone'))).toBe(true);
+		expect(fs.existsSync(path.join(vendorDir, 'linux', 'rclone'))).toBe(true);
+		expect(fs.existsSync(path.join(vendorDir, 'win32', 'rclone.exe'))).toBe(true);
+		expect(fs.existsSync(path.join(vendorDir, 'win64', 'rclone.exe'))).toBe(true);
 	});
 
 	it('includes restic for supported os\'s', () => {
 		const vendorDir = path.join(__dirname, '..', '..', 'vendor');
 
-		expect(fs.existsSync(path.join(vendorDir, 'darwin', 'restic'))).toBeTrue();
-		expect(fs.existsSync(path.join(vendorDir, 'linux', 'restic'))).toBeTrue();
-		expect(fs.existsSync(path.join(vendorDir, 'win32', 'restic.exe'))).toBeTrue();
-		expect(fs.existsSync(path.join(vendorDir, 'win64', 'restic.exe'))).toBeTrue();
+		expect(fs.existsSync(path.join(vendorDir, 'darwin', 'restic'))).toBe(true);
+		expect(fs.existsSync(path.join(vendorDir, 'linux', 'restic'))).toBe(true);
+		expect(fs.existsSync(path.join(vendorDir, 'win32', 'restic.exe'))).toBe(true);
+		expect(fs.existsSync(path.join(vendorDir, 'win64', 'restic.exe'))).toBe(true);
 	});
 });
